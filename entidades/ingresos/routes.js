b75cf77391734809cb20b313d013481a8657c360
Luciano Dato <lucianodato82@gmail.com>
2022-01-17 18:47:19 -0300

    coneccion a base de datos exitosa

--- entidades/ingresos/routes.js
+++ entidades/ingresos/routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('mysql');
+const dbConfig = require('../../dbConfig');
 
 router.get('/', (req, res) => {
     res.json("Ruta inicio de ingresos");
@@ -12,5 +14,20 @@ router.get('/consulta', (req, res) => {
 router.get('/consulta2', (req, res) => {
     res.json({ingresos: ["ingresos3", "ingresos4"]});
 });
+
+router.post('/marcas', (req, res) => {
+    const connection = mysql.createConnection(dbConfig);
+    // let marca = req.body.nombre;
+    let marca = "HoziShoes";
+    connection.connect(function (error) {
+        if (error) {
+            throw error;
+        } else {
+            console.log("Conexión establecida");
+        };
+
+        let query = connection.query('INSERT INTO marcas (nombre) VALUES (?)', [marca]);
+    })
+})
 
 module.exports = router;
