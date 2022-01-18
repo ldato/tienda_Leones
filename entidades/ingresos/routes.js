9e4ef519207fc19ba9f4fc17505c0c929a33cb56
Luciano Dato <lucianodato82@gmail.com>
2022-01-18 11:15:05 -0300

    POST marcas y GET consulta funcionando con BBDD

--- entidades/ingresos/routes.js
+++ entidades/ingresos/routes.js
@@ -2,13 +2,28 @@ const express = require('express');
 const router = express.Router();
 const mysql = require('mysql');
 const dbConfig = require('../../dbConfig');
+const bodyParser = require('body-parser');
+const { status } = require('express/lib/response');
+
+router.use(bodyParser.urlencoded({extended: false}));
+router.use(bodyParser.json());
 
 router.get('/', (req, res) => {
     res.json("Ruta inicio de ingresos");
 });
 
 router.get('/consulta', (req, res) => {
-    res.json({ingresos: ["ingreso1", "ingreso2"]});
+    const connection = mysql.createConnection(dbConfig);
+    connection.connect(function (error, result) {
+        if (error) {
+            throw error;
+        } else {
+            connection.query('SELECT * FROM marcas', function (err, result) {
+                if (err) throw err;
+                res.send(result).toString();
+            })
+        }
+    })
 });
 
 router.get('/consulta2', (req, res) => {
@@ -17,17 +32,20 @@ router.get('/consulta2', (req, res) => {
 
 router.post('/marcas', (req, res) => {
     const connection = mysql.createConnection(dbConfig);
-    // let marca = req.body.nombre;
-    let marca = "HoziShoes";
-    connection.connect(function (error) {
+    let marca = req.body.nombre;
+    //let marca = "HoziShoes";
+    connection.connect (function (error, result) {
         if (error) {
             throw error;
         } else {
-            console.log("Conexión establecida");
+            connection.query('INSERT INTO marcas (nombre) VALUES (?)', [marca], function (err, result) {
+                if (err) throw err;
+                res.send(result).toString();
+            });
         };
-
-        let query = connection.query('INSERT INTO marcas (nombre) VALUES (?)', [marca]);
+       
     })
+    
 })
 
 module.exports = router;
