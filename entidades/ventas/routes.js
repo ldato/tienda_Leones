6cac49839c5dfe8b6057807afbd597cdd2269389
Luciano Dato <lucianodato82@gmail.com>
2022-01-21 19:26:25 -0300

    endpoint de venta escribe en las 2 tablas

--- entidades/ventas/routes.js
+++ entidades/ventas/routes.js
@@ -7,8 +7,61 @@ const bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded({extended: false}));
 router.use(bodyParser.json());
 
+//------------------------------------------PRUEBA DE VENTA CON FUNCION------------------------------------
+
+// const Vender = async () => {
+//     router.post("/venta", (req, res) => {
+//         const connection = mysql.createConnection(dbConfig);
+//         let venta = req.body;
+//         connection.connect(function (error, result) {
+//             try {
+//                 let ventaArray = [
+//                     venta.idClienteDNI,
+                    
+
+//                 ]
+//             } catch (error) {
+//                 throw error;
+//             }
+//         })
+//     })
+// }
+
+//----------------------------------------------------------------------------------------------------------
+
 router.post("/venta", (req, res) => {
-    res.json("Se realizo una venta");
+    const connection = mysql.createConnection(dbConfig);
+    //let venta = req.body;
+    //variables para prueba
+    let venta = [{articulo: "ASD123", cantidad:2}, {articulo:"DSA321", cantidad:1}]
+    let clienteDNI = 12345678;
+    let total = 1000; 
+    let resultQuery = {};
+    
+     connection.connect(async function (error, result) {
+         if (error) {
+             throw error;
+         } else {
+             connection.query('INSERT INTO ventas (idCliente, fecha, total) VALUES (?, NOW(), ?)', 
+             [clienteDNI, total], async function (error, result ) {
+                 if (error) throw error;
+                 resultQuery = await result;
+                 res.send(resultQuery).toString();
+                 console.log(resultQuery);
+                 for (let i = 0; i < venta.length; i++) {
+                     connection.query('INSERT INTO ventasxarticulo VALUES (?, ?, ?)',
+                     [resultQuery.insertId, venta[i].articulo, venta[i].cantidad], async function (error, result) {
+                         if (error) throw error;
+                         await result;
+                          //res.send(result).toString();
+                          //console.log(result);
+                     } )
+                     
+                 }
+             } )
+         }
+     })
+    
 })
 
 // router.get("/ventaXcliente", (req, res) => {
