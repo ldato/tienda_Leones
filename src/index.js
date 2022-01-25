9c6d8c277b4aea50c5312a65c233e7418c84b3f1
Luciano Dato <lucianodato82@gmail.com>
2022-01-25 19:17:47 -0300

    Se agrego cors para pruebas locales

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const router = require('./router');
+const cors = require('cors');
+
+
 
 const app = express();
-const port = 3000;
+const port = 3001;
+
+app.use(cors());
 
 router(app);
 
